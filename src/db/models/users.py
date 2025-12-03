6ae268baed2b8f7d14420f13c63d6d750f2b4fa3
G.Perdikaris <perdgeorge@gmail.com>
2025-12-04 01:53:50 +0200

    import users db model

---
+++ src/db/models/users.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from src.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(50), nullable=False)
