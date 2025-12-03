4fb16de048ee11db8fea654678df5a74f6324de7
G.Perdikaris <perdgeorge@gmail.com>
2025-12-04 01:49:42 +0200

    import config

---
+++ src/core/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class BaseConfig(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", extra="ignore")
+
+
+class Config(BaseConfig):
+    APP_NAME: str = "App Name"
+    POSTGRES_HOST: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    DB_URL: str
+
+
+def get_config(env_state):
+    """Instantiate config based on the environment."""
+    return Config()
+
+
+config = get_config(BaseConfig())
