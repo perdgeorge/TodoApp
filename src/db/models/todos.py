082596c4afb76c283d16516427065fddf86984c5
G.Perdikaris <perdgeorge@gmail.com>
2025-12-04 01:53:36 +0200

    Import Todo db model

---
+++ src/db/models/todos.py
@@ -0,0 +1,11 @@
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+from src.db.base import Base
+
+
+class Todo(Base):
+    __tablename__ = "todos"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    description: Mapped[str] = mapped_column(String(1000), nullable=False)
