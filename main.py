f6546f3f024274d67706388ce5dd8bfbbb723fd3
G.Perdikaris <perdgeorge@gmail.com>
2025-12-05 02:40:46 +0200

    import todos router in main

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from src.api.users.routes import router as users_router
-
+from src.api.todos.routes import router as todos_router
 
 app = FastAPI()
 
 app.include_router(users_router, prefix="/users", tags=["users"])
+app.include_router(todos_router, prefix="/todos", tags=["todos"])
 
 
 @app.get("/health")
