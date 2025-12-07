c373e09c98c6a3c82325084e73f0bb1eeba29c0f
G.Perdikaris <perdgeorge@gmail.com>
2025-12-07 04:12:05 +0200

    Add unique property in todos title

--- src/db/models/todos.py
+++ src/db/models/todos.py
@@ -1,11 +1,12 @@
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
-from src.db.base import Base
+
+from src.db.base import Base, TimestampMixin
 
 
-class Todo(Base):
+class Todo(Base, TimestampMixin):
     __tablename__ = "todos"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    title: Mapped[str] = mapped_column(String(100), nullable=False)
+    title: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     description: Mapped[str] = mapped_column(String(1000), nullable=False)
