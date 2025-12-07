76da06991ed29fad1aae5db14e28b77e2d0e326e
G.Perdikaris <perdgeorge@gmail.com>
2025-12-07 04:12:24 +0200

    Import TimestampMixin

--- src/db/base.py
+++ src/db/base.py
@@ -1,5 +1,14 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+from sqlalchemy import DateTime
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.sql import func
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class TimestampMixin:
+    completed_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now()
+    )
