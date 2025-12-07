8420ab0b8349e9f710a95c9a23279a244d839dab
G.Perdikaris <perdgeorge@gmail.com>
2025-12-07 04:12:40 +0200

    Fix string size for password in user

--- src/db/models/users.py
+++ src/db/models/users.py
@@ -8,4 +8,4 @@ class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    hashed_password: Mapped[str] = mapped_column(String(50), nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
