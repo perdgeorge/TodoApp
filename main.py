8a8e6f942b0b3f3f81f5fb391097bac84e1c30b5
G.Perdikaris <perdgeorge@gmail.com>
2025-12-09 05:20:00 +0200

    Add logging and auth router

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from src.api.users.routes import router as users_router
 from src.api.todos.routes import router as todos_router
+from src.api.auth.routes import router as auth_router
+from src.core.logging import setup_logging
 from src.core.schemas import ErrorSchema
 from src.core.exceptions import ErrorException
 
-app = FastAPI()
+setup_logging()
 
-app.include_router(users_router, prefix="/users", tags=["users"])
-app.include_router(todos_router, prefix="/todos", tags=["todos"])
+app = FastAPI()
 
 
 @app.exception_handler(ErrorException)
@@ -23,6 +24,11 @@ async def exception_handler(request: Request, exc: ErrorException):
         status_code=exc.code,
         content=error_response.as_exception_response(),
     )
+
+
+app.include_router(users_router, prefix="/users", tags=["users"])
+app.include_router(todos_router, prefix="/todos", tags=["todos"])
+app.include_router(auth_router, tags=["auth"])
 
 
 @app.get("/health")
